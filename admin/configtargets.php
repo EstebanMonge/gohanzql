ea48e09d10cb2ccc629a18f3e82c79f4dec0dab2
wzowim00 <dgb.2018@wizonet.ch>
2022-12-26 17:28:40 +0100

    Final 3.5.0 version for configtargets.php and configtargets.htm.tpl

--- admin/configtargets.php
+++ admin/configtargets.php
@@ -2,7 +2,7 @@
 /* ----------------------------------------------------------------------------
  NagiosQL
 -------------------------------------------------------------------------------
- (c) 2005-2022 by Martin Willisegger
+ (c) 2005-2023 by Martin Willisegger
 
  Project   : NagiosQL
  Component : Admin configuration target administration
@@ -19,58 +19,56 @@ use functions\NagVisualClass;
 
 /**
  * Class and variable includes
- * @var HTML_Template_IT $conttp
- * @var HTML_Template_IT $maintp
- * @var HTML_Template_IT $mastertp
- * @var NagVisualClass $myVisClass
- * @var NagConfigClass $myConfigClass
- * @var NagDataClass $myDataClass
- * @var NagContentClass $myContentClass
- * @var MysqliDbClass $myDBClass
- * @var string $chkModus from prepend_adm.php
- * @var int $chkDataId from prepend_adm.php
- * @var string $hidSortDir from prepend_adm.php
- * @var int $hidSortBy from prepend_adm.php
- * @var int $chkLimit from prepend_adm.php
- * @var array $SETS from prepend_adm.php
- * @var string $strConsistMessage from prepend_adm.php
- * @var string $setFileVersion from prepend_adm.php
- * @var string $chkTfValue1 from prepend_content.php
- * @var string $chkTfValue2 from prepend_content.php
- * @var string $chkTfValue4 from prepend_content.php
- * @var string $chkTfValue5 from prepend_content.php
- * @var string $chkTfValue6 from prepend_content.php
- * @var string $chkTfValue7 from prepend_content.php
- * @var string $chkTfValue8 from prepend_content.php
- * @var string $chkTfValue9 from prepend_content.php
- * @var string $chkTfValue10 from prepend_content.php
- * @var string $chkTfValue11 from prepend_content.php
- * @var string $chkTfValue12 from prepend_content.php
- * @var string $chkTfValue13 from prepend_content.php
- * @var string $chkTfValue14 from prepend_content.php
- * @var string $chkTfValue15 from prepend_content.php
- * @var string $chkTfValue16 from prepend_content.php
- * @var string $chkTfValue17 from prepend_content.php
- * @var string $chkTfValue18 from prepend_content.php
- * @var string $chkTfValue19 from prepend_content.php
- * @var string $chkTfValue20 from prepend_content.php
- * @var string $chkTfValue21 from prepend_content.php
- * @var string $chkTfValue22 from prepend_content.php
- * @var string $chkTfValue23 from prepend_content.php
- * @var int $chkSelValue1 from prepend_content.php
- * @var int $chkSelValue2 from prepend_content.php
- * @var int $chkChbValue1 from prepend_content.php
- * @var int $chkSelAccGr from prepend_content.php
- * @var int $chkSelModify from prepend_content.php
- * @var int $chkActive from prepend_content.php
- * @var string $strAccess from prepend_content.php
- *
- *
- * @var int $intGlobalWriteAccess from prepend_content.php
- * @var int $intWriteAccessId from prepend_content.php
- * /*
- * Path settings
+ * @var HTML_Template_IT $conttp Content template
+ * @var HTML_Template_IT $maintp Main template
+ * @var HTML_Template_IT $mastertp Master template (list view)
+ * @var MysqliDbClass $myDBClass MySQL database class
+ * @var NagVisualClass $myVisClass Visual content class
+ * @var NagDataClass $myDataClass NagiosQL data class
+ * @var NagContentClass $myContentClass NagiosQL content class
+ * @var NagConfigClass $myConfigClass NagiosQL configuration class
+ * @var string $setFileVersion from prepend_adm.php -> Application version string
+ * @var string $chkModus from prepend_adm.php -> Form work mode
+ * @var int $chkDataId from prepend_adm.php -> Actual dataset id
+ * @var string $chkSelModify from prepend_adm.php -> Modification selection value
+ * @var int $hidSortBy from prepend_adm.php -> Sort data by
+ * @var string $hidSortDir from prepend_adm.php -> Sort data direction (ASC, DESC)
+ * @var int $chkLimit from prepend_adm.php / settings -> Data set count per page
+ * @var array $SETS Settings array
+ * @var int $intGlobalWriteAccess from prepend_content.php -> Global admin write access
+ * @var int $intWriteAccessId from prepend_content.php -> Admin write access to actual dataset id
+ * @var string $strAccess from prepend_content.php -> List of read access group id's for actual user
+ * @var string $chkTfValue1 from prepend_content.php -> Configuration target name
+ * @var string $chkTfValue2 from prepend_content.php -> Configuration target description
+ * @var string $chkTfValue4 from prepend_content.php -> Server name
+ * @var string $chkTfValue5 from prepend_content.php -> User name
+ * @var string $chkTfValue6 from prepend_content.php -> Password
+ * @var string $chkTfValue7 from prepend_content.php -> ssh key file path
+ * @var string $chkTfValue8 from prepend_content.php -> Base directory
+ * @var string $chkTfValue9 from prepend_content.php -> Host configuration directory
+ * @var string $chkTfValue10 from prepend_content.php -> Service configuration directory
+ * @var string $chkTfValue11 from prepend_content.php -> Backup directory
+ * @var string $chkTfValue12 from prepend_content.php -> Host backup directory
+ * @var string $chkTfValue13 from prepend_content.php -> Service backup directory
+ * @var string $chkTfValue14 from prepend_content.php -> Nagios base directory
+ * @var string $chkTfValue15 from prepend_content.php -> Import directory
+ * @var string $chkTfValue16 from prepend_content.php -> Picture directory
+ * @var string $chkTfValue17 from prepend_content.php -> Command file
+ * @var string $chkTfValue18 from prepend_content.php -> Binary file
+ * @var string $chkTfValue19 from prepend_content.php -> Nagios PID file
+ * @var string $chkTfValue20 from prepend_content.php -> Nagios configuration file
+ * @var string $chkTfValue21 from prepend_content.php -> CGI configuration file
+ * @var string $chkTfValue22 from prepend_content.php -> Ressource file
+ * @var string $chkTfValue23 from prepend_content.php -> ssh port
+ * @var string $chkActive from prepend_content.php -> Active checkbox
+ * @var int $chkSelValue1 from prepend_content.php -> Configuration access method (file based, ssh, ftp)
+ * @var int $chkSelValue2 from prepend_content.php -> Nagios version
+ * @var int $chkChbValue1 from prepend_content.php -> Use secure ftp
+ * @var int $chkSelAccGr from prepend_content.php -> Access group selector
  */
+
+
+
 $strPattern = '(admin/[^/]*.php)';
 $preRelPath = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'PHP_SELF'));
 $preBasePath = preg_replace($strPattern, '', filter_input(INPUT_SERVER, 'SCRIPT_FILENAME'));
@@ -88,6 +86,7 @@ $intIsError = 0;
 $strPathMessage = '';
 $strErrorMessage = '';
 $strInfoMessage = '';
+$strConsistMessage = '';
 /*
 Include preprocessing file
 */
@@ -123,7 +122,7 @@ if (($chkModus === 'modify' || $chkModus === 'insert') && $chkDataId !== 0) {
                 $intIsError = 1;
             }
         }
-        // Nagios base configuration files
+        /* Nagios base configuration files */
         if (!is_writable($chkTfValue20)) {
             $myVisClass->processMessage(str_replace('  ', ' ', translate('Nagios config file') . ' ' . $chkTfValue20
                     . ' ' .translate('is not writeable')), $strPathMessage);
@@ -162,12 +161,12 @@ if (($chkModus === 'modify' || $chkModus === 'insert') && $chkDataId !== 0) {
             $intIsError = 1;
         }
     }
-    // Check SSH Method
+    /* Check SSH Method */
     if (($chkSelValue1 === 3) && !function_exists('ssh2_connect')) {
         $myVisClass->processMessage(translate('SSH module not loaded!'), $strPathMessage);
         $intIsError = 1;
     }
-    // Check FTP Method
+    /* Check FTP Method */
     if (($chkSelValue1 === 2) && !function_exists('ftp_connect')) {
         $myVisClass->processMessage(translate('FTP module not loaded!'), $strPathMessage);
         $intIsError = 1;
@@ -246,7 +245,7 @@ if ($chkModus === 'add') {
     $conttp->setVariable('FILL_ALLFIELDS', translate('Please fill in all fields marked with an *'));
     $conttp->setVariable('FILL_ILLEGALCHARS', translate('The following field contains illegal characters:'));
     /* Insert data from database in "modify" mode */
-    if (isset($arrModifyData) && ((string)$chkSelModify === 'modify')) {
+    if (isset($arrModifyData) && ($chkSelModify === 'modify')) {
         /* Process data */
         $myContentClass->addInsertData($conttp, $arrModifyData, 0, '');
         /* Connection method */
@@ -280,7 +279,7 @@ if ($chkModus === 'add') {
             $conttp->setVariable('LOCKCLASS', 'inputlock');
         }
     }
-    if ((string)$chkSelModify === 'errormodify') {
+    if ($chkSelModify === 'errormodify') {
         $conttp->setVariable('DAT_TARGET', $chkTfValue1);
         /* Domain localhost can't be renamed */
         if ($chkTfValue1 === 'localhost') {
