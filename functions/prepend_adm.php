7e40440fa5e0a4a81a3a40c2895b248bfabcdd84
wzowim00 <dgb.2018@wizonet.ch>
2023-03-03 18:48:30 +0100

    New GIT version string

--- functions/prepend_adm.php
+++ functions/prepend_adm.php
@@ -43,7 +43,7 @@ $intError = 0;
 $setEnableCommon = 0;
 $setDBVersion = 'unknown';
 $setFileVersion = '3.5.0-beta';
-$setGITVersion = '2023-02-04';
+$setGITVersion = '2023-03-03';
 $arrLocale = array();
 /*
 Start PHP session
